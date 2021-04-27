182b67e390a413a502eefe34a711150c852462fc
mjsolidarios <bashdev7@gmail.com>
2021-04-27 20:23:24 +0800

    Allow cors

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "https://sync-volume-server.herokuapp.com/",
+    origin: "*",
     methods: ["GET", "POST"]
   }
 });
